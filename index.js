72c7fcc4b698e228cc2e9a41e283877ed44beeff
ricardocicerodasilva <ricardocixero@gmail.com>
2024-09-24 22:08:17 -0300

    aula24/09

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const { default: mongoose} =require('mongoose')
 const Person=require("./models/Person");
-const app = express()
+const app = express();
 
 
 app.use(
@@ -52,4 +52,63 @@ mongoose.connect("mongodb://localhost:27017").then(()=>{
 })
 .catch((err)=>{
     console.log(err)
+})
+// Read by id
+app .get("/person/:id",async (req, res)=>{
+    const id = req.params.id
+    try{
+        const peaple = await Person.findOne({_id: id})
+        if(!peaple){
+            res.status(422).json({message:"usuario não encontrado!"})
+            return
+        }
+        res.status(200).json(peaple)
+            }catch(error){
+                res.status(500).json({erro: error})
+            }
+})
+
+//update
+app.patch("/person/:id", async (req, res)=>{
+    const id = req.params.id
+
+    const{name,salary,approved}=req.body
+
+    const person={
+        name,
+        salary,
+        approved
+    }
+    try{
+        const updatePerson=await Person.updateOne({_id:id},person)
+
+        if(updatePerson.matchedCount===0){
+            res.status(422).json({message:"usuario não encontrado"})
+        return        
+        }
+        res.status(200).json(person)
+    }catch(error){
+        res.status(500).json({erro: error})
+    }
+})
+//delete
+
+app.delete("/person/:id", async (req, res)=>{
+
+    const id = req.params.id
+    const person=await Person.findOne({_id: id})
+       
+    if(!person){
+        res.status(422).json({message:"usuario não encontrado"})
+        return  
+    }
+    try{
+        await Person.deleteOne({_id:id})
+        res.status(200).json({message:"usuario removido com sucesso"})
+                     
+        
+       
+    }catch(error){
+        res.status(500).json({erro: error})
+    }
 })
